3deb9bb9ef686c48a968e6e5fdca6b0c14ae4fdc
Hack Reactor Students <joe.dou@gmail.com>
2014-05-08 22:08:09 -0700

    SQL and promises update

--- web/basic-server.js
+++ web/basic-server.js
@@ -4,17 +4,50 @@ var handler = require("./request-handler");
 var fetch = require('../workers/htmlfetcher.js');
 
 var cronJob = require('cron').CronJob;
-var job = new cronJob('*/1 * * * * *', function(){
+var job = new cronJob('0 */1 * * * *', function(){
   fetch.fetchSites()
 
   }, function () {
-    // This function is executed when the job stops
+    console.log('cron job complete');
   },
-  true /* Start the job right now */,
-  timeZone /* Time zone of this job. */
+  true /* Start the job right now */
 );
 
 job.start();
+
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'webhistoriantest',
+  password : 'testpassword'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
+});
+// connection.query('INSERT INTO webHistorian.webHistorianTable (website, updated) Values ("www.google.com", true)', function(err, results){
+//   if (err){
+//     console.log('sql error: ' + err);
+//     throw(err);
+//   } else {
+//     console.log('sql results: ' + results);
+//   }
+// });
+
+connection.query('SELECT * from webHistorian.webHistorianTable', function(err, rows, fields){
+  if (err){
+    console.log('sql error: ' + err);
+    throw(err);
+  } else {
+    console.log('sql row results: ' + rows[0].website);
+    console.log('sql fields results: ' + JSON.stringify(fields));
+
+  }
+});
 
 
 var port = 8080;
--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,6 +1,7 @@
 // eventually, you'll have some code here that uses the code in `archive-helpers.js`
 // to actually download the urls you want to download.
 
+var q = require('q');
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
@@ -8,6 +9,8 @@ var request = require('http-request');
 var helper = require('/Users/student/code/joedou/2014-04-web-historian/helpers/archive-helpers.js');
 
 console.log('type of archive: ', typeof helper.paths);
+// var readFile = q.denodeify(fs.readFile);
+// var fileExist = q.denodeify(fs.exists);
 
 exports.fetchSites = function(){
   helper.readListOfUrls(function(arrayOfSites){
@@ -20,3 +23,16 @@ exports.fetchSites = function(){
     });
   });
 };
+
+// exports.fetchSites = function(){
+//   readFile(helper.paths.list)
+//     .then(function(data){
+//       var dataArr = data.toString().split('\n');
+//       console.log('read site File', dataArr);
+//       return dataArr;
+//     })
+//     .then(function(dataArr){
+//       console.log(dataArr);
+//     });
+//   console.log('outside of each');
+// };
